32f3633949e6a82b3f750c2268894ed32aa8bb49
josekuaan <joshuaifeanyi67@gmail.com.com>
2022-02-04 16:17:58 -0800

    worked on fourth section

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -7,7 +7,7 @@ import sinuRider from '../images/sinu-rider.png';
 import sinuGhost from '../images/sinu-ghost.png';
 import sinuWolve from '../images/sinu-wolve.png';
 import meetTeam from '../images/meet-the-team.png'
-import fighter1 from '../images/Fighter 1.png'
+import SINUVERSE1 from '../images/sinuverse_1.mp4'
 
 export default function Teams() {
   return <div className='sinu'>
@@ -32,18 +32,22 @@ export default function Teams() {
                <img src={sinuWolve} alt='sinu-wolve'  />
            </div>
         </section>
-        <div className=' section section-three'>
+        <div className=' section section-sinuverse'>
                         
-            <div className='section-three-container'>
-                
+            <div className='section-three-sinuverse-text'>
+            
                 <p > 
                     PvP(Player vs Player ) Mode The most advanced mode of our Play-2-Earn game is to allow players compete together in this player vs player mode and the goal is to increase community interaction through gaming.<br /><br /> PvC(Player Vs Computer) This can have massive effect on the clans leaderboard as playing against the computer might prove to be a more challenging opponent compared to the PvP mode. For fairness, we encourage that players engage in the PvP mode.
                 </p>
                 
             </div>
             
-            <div className='section-three-img'>
-                <img src={fighter1} alt='' style={{width:'100%'}} />
+            <div className='section-three-vedio'>
+            <video  autoPlay muted>
+                <source src={SINUVERSE1} width={500} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+                
             </div>
         </div>
   </div>;
